1437fae92a76351c77a2909bb81cd1240c94aa7e
Виноградов Юрий Викторович <yvinogradov@n-core.ru>
2018-03-14 16:39:32 +0300

    - Errors

--- src/main/java/com/github/yurivin/blockchain/blockjane/infrastracture/Block.java
+++ src/main/java/com/github/yurivin/blockchain/blockjane/infrastracture/Block.java
@@ -4,21 +4,20 @@ import java.util.Date;
 
 public class Block {
 
-    public Block(Environment env) {
+    public Block(Environment env, long timeStamp, String data, String previousHash, String hash) {
         this.env = env;
-    }
-    public final String hash;
-    public final String previousHash;
-    private final String data; //our data will be a simple message.
-    private final long timeStamp; //as number of milliseconds since 1/1/1970 in UTC.
-    public final Environment env;
-
-    //Block Constructor.
-    public Block(String data,String previousHash ) {
+        this.timeStamp = timeStamp;
         this.data = data;
         this.previousHash = previousHash;
-        this.timeStamp = new Date().getTime();
+        this.hash = hash;
     }
+    private final String hash;
+    private final String previousHash;
+    private final String data; //our data will be a simple message.
+    private final long timeStamp; //as number of milliseconds since 1/1/1970 in UTC.
+    private final Environment env;
+
+    //Block Constructor.
 
     public String calculateHash() {
         String calculatedhash = env.hashAlgo.apply(
--- src/test/java/com/github/yurivin/blockchain/blockjane/TestBlockchain.java
+++ src/test/java/com/github/yurivin/blockchain/blockjane/TestBlockchain.java
@@ -1,12 +1,13 @@
 package com.github.yurivin.blockchain.blockjane;
 
+import com.github.yurivin.blockchain.blockjane.hash.algorithm.SHA256;
 import org.junit.Test;
 
 public class TestBlockchain {
 
     @Test
     public void mainTest() {
-        Blockchain blockchain = new Blockchain();
+        Blockchain blockchain = new Blockchain(new SHA256());
 
     }
 }
